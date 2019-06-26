f33f6e1c582de6640a9a51a21a133c5197d80d17
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-06-26 19:50:26 +0200

    Updates

---
+++ precache-manifest.5ede06393f54a5c95a00dc0d209d976d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2376657f656b447c3d4b",
+    "url": "/sudoku/static/js/runtime~main.e7756cf6.js"
+  },
+  {
+    "revision": "423a7d552c083021afde",
+    "url": "/sudoku/static/js/main.433febeb.chunk.js"
+  },
+  {
+    "revision": "c3c3f0c62320b6b05a07",
+    "url": "/sudoku/static/js/2.5fd13874.chunk.js"
+  },
+  {
+    "revision": "423a7d552c083021afde",
+    "url": "/sudoku/static/css/main.4b5374c4.chunk.css"
+  },
+  {
+    "revision": "f3eb3592785ba245d15f7828b0115c36",
+    "url": "/sudoku/index.html"
+  }
+];
