45cd3c210d0268f92d0b3f8e6e023d29f502e2a9
Przemyslaw Przewoznik <blagoff88@gmail.com>
2020-01-19 23:11:35 +0100

    fix flex rwd

---
+++ build/precache-manifest.a081bda622d14472d7d68d6e5d19bda8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "423a7d552c083021afde",
+    "url": "/static/js/main.433febeb.chunk.js"
+  },
+  {
+    "revision": "c3c3f0c62320b6b05a07",
+    "url": "/static/js/2.5fd13874.chunk.js"
+  },
+  {
+    "revision": "423a7d552c083021afde",
+    "url": "/static/css/main.4b5374c4.chunk.css"
+  },
+  {
+    "revision": "ca3f1af76b49cd088c33f80c0acf188c",
+    "url": "/index.html"
+  }
+];
