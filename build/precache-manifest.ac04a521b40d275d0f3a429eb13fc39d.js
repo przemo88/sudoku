617c97a04c65fbcd02d87c5c4e59173b609eda94
Przemyslaw Przewoznik <blagoff88@gmail.com>
2020-11-20 21:54:14 +0100

    add rwd

---
+++ build/precache-manifest.ac04a521b40d275d0f3a429eb13fc39d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "98f68ddf9c4c2a84ddeb",
+    "url": "/static/js/main.433febeb.chunk.js"
+  },
+  {
+    "revision": "c3c3f0c62320b6b05a07",
+    "url": "/static/js/2.5fd13874.chunk.js"
+  },
+  {
+    "revision": "98f68ddf9c4c2a84ddeb",
+    "url": "/static/css/main.dde43b0f.chunk.css"
+  },
+  {
+    "revision": "754c0a76861f602b90145d2b691481cb",
+    "url": "/index.html"
+  }
+];
